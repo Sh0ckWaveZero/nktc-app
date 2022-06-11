4efc25ebb23ac3a19ca1bf8dbd6b30c63b167296
Sh0ckWaveZero <ottooffple@icloud.com>
2022-06-11 22:37:58 +0700

    feat: 🎸 prisma and swaggwer

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 declare const module: any;
 
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('NKTC-API')
+    .setDescription('The NKTC API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 
   if (module.hot) {
