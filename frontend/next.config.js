c6638d141b0f8e30a0b3068bacf9adadba926798
Sh0ckWaveZero <ottooffple@icloud.com>
2022-08-09 22:27:25 +0700

    docs: ✏️ Update library version

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,12 +12,12 @@ const withTM = require('next-transpile-modules')([
 ])
 
 module.exports = withTM({
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  },
+  // trailingSlash: true,
+  // reactStrictMode: false,
+  // experimental: {
+  //   esmExternals: false,
+  //   jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
+  // },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
