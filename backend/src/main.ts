8746f7ec86e01b9df4582ee0ed721cf6512d7636
Sh0ckWaveZero <ottooffple@icloud.com>
2022-08-12 23:52:03 +0700

    feat: 🎸 Add Helmet and CSRF Protection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,8 @@ declare const module: any;
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,8 @@ const bootstrap = async () => {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(3001);
 
   if (module.hot) {
