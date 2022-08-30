262e997bec88d7f4e4190e997c16543bec873003
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-08-30 23:26:18 +0700

    refactor: 💡 update utils

--- backend/prisma/db/utils.ts
+++ backend/prisma/db/utils.ts
@@ -22,21 +22,23 @@ export const getBirthday = async (date: string) => {
   const _date = date.substring(0, 2).trim();
   const monthNumber = await Promise.all([
     monthThai
-      .filter((item: string) => date.search(item) !== -1)
+      .filter((item: string) => date.search(item) > -1)
       .map((item: string) => {
         return (monthThai.indexOf(item) + 1).toString().padStart(2, "0");
       })[0],
   ]);
-  const year = await yearThai(date);
+  const year = await getBuddhistYear(date);
   return new Date(`${year}-${monthNumber[0]}-${_date}`);
 }
 
-export const yearThai = async (year: string) => {
-  const year2digit = year.substring(year.length - 2, year.length).trim();
-  const year4digit = year2digit.padStart(4, "25");
-  return parseInt(year4digit) - 543;
+const getBuddhistYear = async (date: string) => {
+  const year = new Date().getFullYear();
+  const buddhistYear = 543;
+  const twoDigitBuddhistYear = (year + buddhistYear).toString().substring(2, 4);
+  const twoDigitYear = date.substring(date.length - 2, date.length).trim();
+  const fourDigitYear = twoDigitYear.padStart(4, twoDigitBuddhistYear);
+  return parseInt(fourDigitYear) - buddhistYear;
 }
-
 
 export const getClassroomId = async (level: string) => {
   const res = await prisma.classroom.findFirst({
@@ -50,10 +52,24 @@ export const getClassroomId = async (level: string) => {
   return res?.classroomId;
 }
 
-export const getPrgramId = async (program: string, level: string) => {
+export const getLevelClassroomId = async (level: string, classroom: string) => {
+  const name = `${level}${classroom}`;
+  console.log("🚀 ~ file: utils.ts ~ line 57 ~ getLevelClassroomId ~ name", name)
+  const res = await prisma.levelClassroom.findFirst({
+    where: {
+      name: name,
+    },
+    select: {
+      levelClassroomId: true,
+    }
+  });
+  return res?.levelClassroomId;
+}
+
+export const getProgramId = async (name: string, level: string) => {
   const res = await prisma.program.findFirst({
     where: {
-      description: program + " " + level,
+      description: name + " " + level,
     },
     select: {
       programId: true,
@@ -62,7 +78,7 @@ export const getPrgramId = async (program: string, level: string) => {
   return res?.programId;
 }
 
-export const readWorkSheeFromFile = (path: string) => {
+export const readWorkSheetFromFile = (path: string) => {
   const workSheetsFromFile = xlsx.parse(
     fs.readFileSync(`${__dirname}/import/${path}.xlsx`)
   );
