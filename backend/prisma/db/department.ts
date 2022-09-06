b49b4cd77fedfa4df79d14392bee655fde134273
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-09-06 23:13:34 +0700

    refactor: 💡 Update department

--- backend/prisma/db/department.ts
+++ backend/prisma/db/department.ts
@@ -1,83 +1,26 @@
 import { Prisma } from '@Prisma/client'
-import { createByAdmin } from './utils';
+import { createByAdmin, readWorkSheetFromFile } from './utils';
 
-export const departmentData = () => {
+export const departmentData = async () => {
+  const workSheetsFromFile = readWorkSheetFromFile('department');
   const admin = createByAdmin();
 
-  return Prisma.validator<Prisma.DepartmentCreateInput[]>()(
-    [
-      {
-        departmentId: "D001",
-        name: "ช่างกลโรงงาน",
-        description: "แผนก ช่างกลโรงงาน",
-        ...admin,
-      },
-      {
-        departmentId: "D002",
-        name: "ช่างเชื่อมโลหะ",
-        description: "แผนก ช่างเชื่อมโลหะ",
-        ...admin,
-      },
-      {
-        departmentId: "D003",
-        name: "ช่างไฟฟ้ากำลัง",
-        description: "แผนก ช่างไฟฟ้ากำลัง",
-        ...admin,
-      },
-      {
-        departmentId: "D004",
-        name: "ช่างอิเล็กทรอนิกส์",
-        description: "แผนก ช่างอิเล็กทรอนิกส์",
-        ...admin,
-      },
-      {
-        departmentId: "D005",
-        name: "โยธา",
-        description: "แผนก โยธา",
-        ...admin,
-      },
-      {
-        departmentId: "D006",
-        name: "เมคคาทรอนิกส์",
-        description: "แผนก เมคคาทรอนิกส์",
-        ...admin,
-      },
-      {
-        departmentId: "D007",
-        name: "คอมพิวเตอร์ธุรกิจ",
-        description: "แผนก คอมพิวเตอร์ธุรกิจ",
-        ...admin,
-      },
-      {
-        departmentId: "D008",
-        name: "เทคโนโลยีคอมพิวเตอร์",
-        description: "แผนก เทคโนโลยีคอมพิวเตอร์",
-        ...admin,
-      },
-      {
-        departmentId: "D009",
-        name: "ภาษาต่างประเทศธุรกิจ",
-        description: "แผนก ภาษาต่างประเทศธุรกิจ",
-        ...admin,
-      },
-      {
-        departmentId: "D010",
-        name: "เทคโนโลยีสารสนเทศ",
-        description: "แผนก เทคโนโลยีสารสนเทศ",
-        ...admin,
-      },
-      {
-        departmentId: "D011",
-        name: "การจัดการโลจิสติกส์และซัพพลายเชน",
-        description: "แผนก การจัดการโลจิสติกส์และซัพพลายเชน",
-        ...admin,
-      },
-      {
-        departmentId: "D012",
-        name: "ช่างยนต์",
-        description: "แผนก ช่างยนต์",
-        ...admin,
-      },
-    ],
-  );
+  const departments = await Promise.all(workSheetsFromFile[0].data
+    .filter((data: any, id: number) => id > 1 && data)
+    .map(async (item: any) => {
+      const departmentId = item[0].toString();
+      const name = item[1].toString();
+      const description = item[2].toString();
+
+      return Prisma.validator<Prisma.DepartmentCreateInput>()(
+        {
+          departmentId,
+          name,
+          description,
+          ...admin,
+        }
+      );
+    }));
+
+  return departments;
 }
