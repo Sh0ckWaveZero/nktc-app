d462806179e773d778353eea405e29a4d95e05b6
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-11-14 21:42:18 +0700

    refactor: 💡 MiddleWare for get  user agent

--- backend/src/middlewares/logger.middleware.ts
+++ backend/src/middlewares/logger.middleware.ts
@@ -1,14 +1,20 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
-import { NextFunction } from 'express';
+import { NextFunction, Response } from 'express';
 import configuration from 'src/config/configuration';
 import * as requestIp from 'request-ip';
-
+import * as UAParser from 'ua-parser-js';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(LoggerMiddleware.name);
   use(req: any, res: Response, next: NextFunction) {
+
     const userAgent = req.get('User-Agent');
     this.logger.log(userAgent);
+
+    // UAParser
+    const parser = new UAParser(userAgent);
+    const result = parser.getResult();
+    this.logger.log(result);
     this.logger.log(`User ${req?.body?.username} logged in from IP address ${requestIp.getClientIp(req)}`);
     next();
   }
