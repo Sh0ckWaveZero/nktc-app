7fa0629b1071c9fe8be370ec13249d6ec968f425
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-11-14 21:23:51 +0700

    refactor: 💡 move apiUrl to variable

--- frontend/src/configs/auth.ts
+++ frontend/src/configs/auth.ts
@@ -8,20 +8,24 @@ type AuthConfig = {
   classroomEndpoint?: string;
   reportCheckInEndpoint?: string;
   departmentEndpoint?: string;
+  programEndpoint?: string;
   registerEndpoint?: string;
   accessToken?: string;
 }
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export const authConfig: AuthConfig = {
-  meEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/auth/me`,
-  loginEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-  changePasswordEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/auth/update/password`,
-  teacherEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/teachers`,
-  studentEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/students`,
-  appbarEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/app-bar/search`,
-  classroomEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/classrooms`,
-  reportCheckInEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/reportCheckIn`,
-  departmentEndpoint: `${process.env.NEXT_PUBLIC_API_URL}/departments`,
+  meEndpoint: `${apiUrl}/auth/me`,
+  loginEndpoint: `${apiUrl}/auth/login`,
+  changePasswordEndpoint: `${apiUrl}/auth/update/password`,
+  teacherEndpoint: `${apiUrl}/teachers`,
+  studentEndpoint: `${apiUrl}/students`,
+  appbarEndpoint: `${apiUrl}/app-bar/search`,
+  classroomEndpoint: `${apiUrl}/classrooms`,
+  reportCheckInEndpoint: `${apiUrl}/reportCheckIn`,
+  departmentEndpoint: `${apiUrl}/departments`,
+  programEndpoint: `${apiUrl}/programs`,
   registerEndpoint: '/jwt/register',
   accessToken: 'accessToken',
 };
