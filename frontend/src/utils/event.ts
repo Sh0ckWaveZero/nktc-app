5df9d8e52c4e60afd544be4b07ad2dcccdf8b52e
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-12-06 21:19:39 +0700

    feat: 🎸 Add new func handleKeyDown

---
+++ frontend/src/utils/event.ts
@@ -0,0 +1,20 @@
+export const handleKeyDown = (e: any) => {
+  // allow RegExp only number english keyboard control key copy and paste
+  const regex = new RegExp('^[0-9]+$');
+  if (
+    !regex.test(e.key) &&
+    e.key !== 'Backspace' &&
+    e.key !== 'Tab' &&
+    e.key !== 'ArrowLeft' &&
+    e.key !== 'ArrowRight' &&
+    e.key !== 'ArrowUp' &&
+    e.key !== 'ArrowDown' &&
+    e.key !== 'Delete' &&
+    e.key !== 'Control' &&
+    e.key !== 'c' &&
+    e.key !== 'v' &&
+    e.key !== 'x'
+  ) {
+    e.preventDefault();
+  }
+};
