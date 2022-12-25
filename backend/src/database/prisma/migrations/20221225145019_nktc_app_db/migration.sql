fe5cdb7501ef4f99ce2835918600e73b83d60d3d
Aphidet-Hongseetong <ottooffple@icloud.com>
2022-12-25 22:10:33 +0700

    fix: 🐛 docker file

---
+++ backend/src/database/prisma/migrations/20221225145019_nktc_app_db/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `internship` on the `activityCheckInReport` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "activityCheckInReport" DROP COLUMN "internship";
