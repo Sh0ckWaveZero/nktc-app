f5a552dc969c5c03d27b913c6f6c9dbc38043120
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-20 23:08:49 +0700

    Add BadnessIndividualController spec file

---
+++ backend/src/apis/badness-individual/badness-individual.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BadnessIndividualController } from './badness-individual.controller';
+import { BadnessIndividualService } from './badness-individual.service';
+
+describe('BadnessIndividualController', () => {
+  let controller: BadnessIndividualController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [BadnessIndividualController],
+      providers: [BadnessIndividualService],
+    }).compile();
+
+    controller = module.get<BadnessIndividualController>(BadnessIndividualController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
