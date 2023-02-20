df49fb62bd42a653f5a8d906106ccc0925990f75
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-20 23:09:08 +0700

    Add BadnessIndividualModule to provide BadnessIndividualService and PrismaService

---
+++ backend/src/apis/badness-individual/badness-individual.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { BadnessIndividualService } from './badness-individual.service';
+import { BadnessIndividualController } from './badness-individual.controller';
+import { MinioClientModule } from '../minio/minio-client.module';
+import { PrismaService } from 'src/common/services/prisma.service';
+
+@Module({
+  imports: [MinioClientModule],
+  controllers: [BadnessIndividualController],
+  providers: [BadnessIndividualService, PrismaService]
+})
+export class BadnessIndividualModule { }
