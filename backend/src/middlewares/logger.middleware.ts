b75dc6300ff67048c6f2ea069831f42720ab22f7
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-20 08:57:57 +0700

    Logging user login and user agent details to database

--- backend/src/middlewares/logger.middleware.ts
+++ backend/src/middlewares/logger.middleware.ts
@@ -3,19 +3,39 @@ import { NextFunction, Response } from 'express';
 import configuration from 'src/config/configuration';
 import * as requestIp from 'request-ip';
 import * as UAParser from 'ua-parser-js';
+import { PrismaService } from 'src/common/services/prisma.service';
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(LoggerMiddleware.name);
-  use(req: any, res: Response, next: NextFunction) {
 
+  constructor(
+    private readonly prisma: PrismaService,
+  ) { }
+
+  async use(req: any, res: Response, next: NextFunction) {
     const userAgent = req.get('User-Agent');
-    this.logger.log(userAgent);
-
-    // UAParser
     const parser = new UAParser(userAgent);
     const result = parser.getResult();
-    this.logger.log(result);
-    this.logger.log(`User ${req?.body?.username} logged in from IP address ${requestIp.getClientIp(req)}`);
+    const { username, password } = req.body;
+    const ipAddr = requestIp.getClientIp(req);
+
+    // Record log to database
+    await this.prisma.auditLog.create({
+      data: {
+        action: 'Login',
+        model: 'User',
+        fieldName: 'username',
+        oldValue: null,
+        newValue: username,
+        detail: 'User login',
+        ipAddr,
+        browser: result.browser.name,
+        device: result.device.type,
+        createdBy: username,
+      },
+    });
+
     next();
   }
 }
