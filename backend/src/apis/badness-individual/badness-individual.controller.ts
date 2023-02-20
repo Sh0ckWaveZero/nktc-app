99fc661a1f440bd79cb51bd831231772783de35c
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-20 23:08:57 +0700

    Create BadnessIndividualController with POST endpoint

---
+++ backend/src/apis/badness-individual/badness-individual.controller.ts
@@ -0,0 +1,19 @@
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+
+import { BadnessIndividualService } from './badness-individual.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+
+@ApiTags('badness-individual')
+@Controller('badness-individual')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
+export class BadnessIndividualController {
+  constructor(private readonly badnessIndividualService: BadnessIndividualService) {}
+
+  @Post()
+  async create(@Body() createBadnessIndividualDto: any) {
+    return await this.badnessIndividualService.create(createBadnessIndividualDto);
+  }
+
+}
