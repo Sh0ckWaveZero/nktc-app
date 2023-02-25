be2b8295cd1bd5fc8841591f6c5c3f97bd94b2af
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-25 22:43:44 +0700

    Add useStudentList hook to manage students list in frontend

---
+++ frontend/src/hooks/useStudentList.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { shallow } from 'zustand/shallow';
+import { useStudentStore } from '@/store/index';
+import toast from 'react-hot-toast';
+
+const useStudentList = (storedToken: string, debouncedValue: string) => {
+  const { studentsList }: any = useStudentStore(
+    (state: any) => ({
+      fetchStudents: state.fetchStudents,
+      studentsList: state.studentsList,
+    }),
+    shallow,
+  );
+  const [loading, setLoading] = useState(false);
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        setLoading(true);
+        const result = await studentsList(storedToken, { fullName: debouncedValue });
+        setStudents(result || []);
+        setLoading(false);
+      } catch (error: any) {
+        toast.error(error?.message || 'เกิดข้อผิดพลาด');
+        setLoading(false);
+      }
+    };
+    fetchStudents();
+  }, [storedToken, debouncedValue]);
+
+  return { loading, students };
+};
+
+export default useStudentList;
