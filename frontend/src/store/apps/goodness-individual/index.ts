61348468d99192a72cbd0549222de3d961b5d56d
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-25 22:45:18 +0700

    Add search method to GoodnessIndividualStore

--- frontend/src/store/apps/goodness-individual/index.ts
+++ frontend/src/store/apps/goodness-individual/index.ts
@@ -1,14 +1,22 @@
 import { authConfig } from '@/configs/auth';
 import httpClient from '@/@core/utils/http';
 import { create } from 'zustand';
+import { Dayjs } from 'dayjs';
+
+type Body = {
+  fullName?: string;
+  classroomId?: string;
+  goodDate?: Dayjs | null;
+};
 
 interface GoodnessIndividualState {
   createGoodnessIndividual: (token: string, body: any) => any;
+  search: (token: string, body: Body) => any;
 }
 
 export const goodnessIndividualStore = create<GoodnessIndividualState>()(
   () => ({
-    createGoodnessIndividual: async (token: string, body: any) => {
+    createGoodnessIndividual: async (token: string, body: Body) => {
       try {
         const response = await httpClient.post(
           `${authConfig.goodnessIndividualEndpoint}`,
@@ -19,6 +27,22 @@ export const goodnessIndividualStore = create<GoodnessIndividualState>()(
             },
           });
         return response;
+      } catch (err) {
+        console.error('Error creating goodness individual:', err);
+        return err;
+      }
+    },
+    search: async (token: string, body: any) => {
+      try {
+        const response = await httpClient.post(
+          `${authConfig.goodnessIndividualEndpoint}/search`,
+          body,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        return response?.data;
       } catch (err) {
         console.error('Error creating goodness individual:', err);
         return err;
