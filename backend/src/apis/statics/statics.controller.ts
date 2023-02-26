09042e132841b6e633f2a89ca1ee9c5e7bdf8083
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-26 12:09:18 +0700

    Add endpoint for serving badness-individual images

--- backend/src/apis/statics/statics.controller.ts
+++ backend/src/apis/statics/statics.controller.ts
@@ -53,6 +53,16 @@ export class StaticsController {
     await this.serveImage(id, 'goodness-individual/images', response);
   }
 
+  // badness-individual
+  @Get('badness-individual/images/:id')
+  @HttpCode(HttpStatus.OK)
+  async getBadnessIndividual(
+    @Param('id') id: string,
+    @Res() response: Response,
+  ) {
+    await this.serveImage(id, 'badness-individual/images', response);
+  }
+
   private async serveImage(
     id: string,
     prefix: string,
