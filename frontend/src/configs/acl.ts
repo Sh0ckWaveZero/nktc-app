1ae76c280688cf5a0cb772cf5552f25c8d714498
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-02-26 12:09:29 +0700

    Add read permission for report-badness-page for role Student

--- frontend/src/configs/acl.ts
+++ frontend/src/configs/acl.ts
@@ -59,6 +59,7 @@ const defineRulesFor = (role: Role, subject: string) => {
     can(['read'], 'record-goodness-page');
     can(['read'], 'record-badness-page');
     can(['read'], 'report-goodness-page');
+    can(['read'], 'report-badness-page');
   } else if (role === 'Student') {
     can(['read'], 'home-page');
   } else if (role === 'Parent') {
