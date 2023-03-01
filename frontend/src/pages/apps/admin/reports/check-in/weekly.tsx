a10f08897a7f488802e86fc664b282f52dd0ab43
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-01 20:49:15 +0700

    Refactor imports to improve readability of Weekly report page

--- frontend/src/pages/apps/admin/reports/check-in/weekly.tsx
+++ frontend/src/pages/apps/admin/reports/check-in/weekly.tsx
@@ -1,18 +1,20 @@
-// ** React Imports
-import { useState, useEffect } from 'react';
 // ** Types Imports
 import { Avatar, Card, CardHeader, Grid } from '@mui/material';
-import { BsCalendar2Date } from 'react-icons/bs';
-import TableCollapsible from '@/views/apps/admin/reports/check-in/TableCollapsible';
-import { useReportCheckInStore } from '@/store/index';
-import { shallow } from 'zustand/shallow';
-import TableHeaderWeekly from '@/views/apps/admin/reports/check-in/TableHeaderWeekly';
 import dayjs, { Dayjs } from 'dayjs';
+// ** React Imports
+import { useEffect, useState } from 'react';
+
+import { BsCalendar2Date } from 'react-icons/bs';
 import { LocalStorageService } from '@/services/localStorageService';
 import { ReportCheckIn } from '@/types/apps/reportCheckIn';
 import Spinner from '@/@core/components/spinner';
+import TableCollapsible from '@/views/apps/admin/reports/check-in/TableCollapsible';
+import TableHeaderWeekly from '@/views/apps/admin/reports/check-in/TableHeaderWeekly';
+import buddhistEra from 'dayjs/plugin/buddhistEra';
 import { isEmpty } from '@/@core/utils/utils';
-
+import { shallow } from 'zustand/shallow';
+import { useReportCheckInStore } from '@/store/index';
+dayjs.extend(buddhistEra);
 
 const localStorageService = new LocalStorageService();
 
