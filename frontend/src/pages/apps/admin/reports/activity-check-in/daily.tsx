f4c690cdc72dc663ea79a666929a9bdb16fa0277
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-01 20:48:28 +0700

    Add buddhistEra plugin to activity check-in daily page

--- frontend/src/pages/apps/admin/reports/activity-check-in/daily.tsx
+++ frontend/src/pages/apps/admin/reports/activity-check-in/daily.tsx
@@ -6,14 +6,15 @@ import { BsCalendar2Date } from 'react-icons/bs';
 import { LocalStorageService } from '@/services/localStorageService';
 import { ReportCheckIn } from '@/types/apps/reportCheckIn';
 import Spinner from '@/@core/components/spinner';
-
+import TableCollapsible from '@/views/apps/admin/reports/activity-check-in/TableCollapsible';
 import TableHeader from '@/views/apps/admin/reports/check-in/TableHeader';
+import buddhistEra from 'dayjs/plugin/buddhistEra';
 import { isEmpty } from '@/@core/utils/utils';
 import { shallow } from 'zustand/shallow';
 import { useActivityCheckInStore } from '@/store/index';
-import TableCollapsible from '@/views/apps/admin/reports/activity-check-in/TableCollapsible';
 
 dayjs.locale('th');
+dayjs.extend(buddhistEra);
 
 const localStorageService = new LocalStorageService();
 
