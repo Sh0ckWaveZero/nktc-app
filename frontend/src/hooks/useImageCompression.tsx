007e89948b9f4dffe59d367a14759699f7d3c8c0
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-01 20:48:11 +0700

    Refactor useImageCompression hook to use promises for image compression handling

--- frontend/src/hooks/useImageCompression.tsx
+++ frontend/src/hooks/useImageCompression.tsx
@@ -4,20 +4,18 @@ import imageCompression from 'browser-image-compression';
 const useImageCompression = () => {
   const [imageCompressed, setImage] = useState('');
 
-  const handleInputImageChange = async (event: any) => {
+  const handleInputImageChange = (event: any) => {
     const imageFile = event.target.files[0];
     const options = {
       maxSizeMB: 0.3,
       maxWidthOrHeight: 1920,
       useWebWorker: true,
     };
-    try {
-      const compressedFile = await imageCompression(imageFile, options);
-      const image64Base = await imageCompression.getDataUrlFromFile(compressedFile);
-      setImage(image64Base);
-    } catch (error) {
-      console.error(error);
-    }
+
+    imageCompression(imageFile, options)
+      .then((compressedFile) => imageCompression.getDataUrlFromFile(compressedFile))
+      .then((image64Base) => setImage(image64Base))
+      .catch((error) => console.error(error));
   };
 
   return { imageCompressed, handleInputImageChange };
