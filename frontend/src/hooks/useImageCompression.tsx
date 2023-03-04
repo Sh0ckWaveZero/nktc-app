98e177194eb760b2b3679a2a7fdba35aa73cf334
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-04 14:16:21 +0700

    Update useImageCompression hook to include isCompressing status and handle errors

--- frontend/src/hooks/useImageCompression.tsx
+++ frontend/src/hooks/useImageCompression.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import imageCompression from 'browser-image-compression';
 
 const useImageCompression = () => {
-  const [imageCompressed, setImage] = useState('');
+  const [imageCompressed, setImageCompressed] = useState('');
+  const [isCompressing, setIsCompressing] = useState(false);
 
-  const handleInputImageChange = (event: any) => {
+  const handleInputImageChange = async (event: any) => {
+    setIsCompressing(true);
     const imageFile = event.target.files[0];
     const options = {
       maxSizeMB: 0.3,
@@ -12,13 +14,18 @@ const useImageCompression = () => {
       useWebWorker: true,
     };
 
-    imageCompression(imageFile, options)
-      .then((compressedFile) => imageCompression.getDataUrlFromFile(compressedFile))
-      .then((image64Base) => setImage(image64Base))
-      .catch((error) => console.error(error));
+    try {
+      const compressedFile = await imageCompression(imageFile, options);
+      const image64Base = await imageCompression.getDataUrlFromFile(compressedFile);
+      setImageCompressed(image64Base);
+      setIsCompressing(false);
+    } catch (error) {
+      console.error(error);
+      setIsCompressing(false);
+    }
   };
 
-  return { imageCompressed, handleInputImageChange };
+  return { imageCompressed, handleInputImageChange, isCompressing };
 };
 
 export default useImageCompression;
