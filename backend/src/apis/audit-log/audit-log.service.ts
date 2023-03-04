93d362600e2e3522e1e979976d23dcf9566aa01c
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-04 14:19:18 +0700

    Update AuditLogService to use PrismaService for finding auditLogs

--- backend/src/apis/audit-log/audit-log.service.ts
+++ backend/src/apis/audit-log/audit-log.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuditLogDto } from './dto/create-audit-log.dto';
-import { UpdateAuditLogDto } from './dto/update-audit-log.dto';
+import { PrismaService } from 'src/common/services/prisma.service';
+
 
 @Injectable()
 export class AuditLogService {
-  create(createAuditLogDto: CreateAuditLogDto) {
-    return 'This action adds a new auditLog';
-  }
 
-  findAll() {
-    return `This action returns all auditLog`;
-  }
+  constructor(
+    private readonly prisma: PrismaService,
+  ) { }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auditLog`;
-  }
-
-  update(id: number, updateAuditLogDto: UpdateAuditLogDto) {
-    return `This action updates a #${id} auditLog`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auditLog`;
+  async findOne(username: string) {
+    return await this.prisma.auditLog.findMany({
+      where: {
+        createdAt: username
+      },
+    });
   }
 }
