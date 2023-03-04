e329ea8297d1792a49e656041cf168ac5d030520
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-04 14:19:27 +0700

    Add MinioClientModule to TeachersModule

--- backend/src/apis/teachers/teachers.module.ts
+++ backend/src/apis/teachers/teachers.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { TeachersController } from './teachers.controller';
 import { PrismaService } from 'src/common/services/prisma.service';
+import { MinioClientModule } from '../minio/minio-client.module';
 
 @Module({
+  imports: [MinioClientModule],
   controllers: [TeachersController],
   providers: [TeachersService, PrismaService],
 })
-export class TeachersModule {}
+export class TeachersModule { }
