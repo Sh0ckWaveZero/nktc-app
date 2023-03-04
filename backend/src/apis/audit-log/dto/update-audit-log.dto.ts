fe978cad7d63cbf44c6388a335534e13ef360170
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-04 14:18:45 +0700

    Remove UpdateAuditLogDto as it is unnecessary

--- backend/src/apis/audit-log/dto/update-audit-log.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAuditLogDto } from './create-audit-log.dto';
-
-export class UpdateAuditLogDto extends PartialType(CreateAuditLogDto) {}
