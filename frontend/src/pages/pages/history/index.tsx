32ee295971dc2b5528727d5dc6eefab9bd53e54b
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-05 19:56:56 +0700

    Add HistoryPage component with ACL action and subject

---
+++ frontend/src/pages/pages/history/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const HistoryPage = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+
+HistoryPage.acl = {
+  action: 'read',
+  subject: 'history-page',
+}  
+
+export default HistoryPage
