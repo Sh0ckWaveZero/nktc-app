e75b618bf44a8d2e4b571bca7d7736074998101f
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-05 19:55:22 +0700

    refactor: 💡 VerticalNavLink

--- frontend/src/@core/layouts/components/vertical/navigation/VerticalNavLink.tsx
+++ frontend/src/@core/layouts/components/vertical/navigation/VerticalNavLink.tsx
@@ -1,52 +1,50 @@
 // ** React Imports
-import { ElementType, ReactNode } from 'react';
+import { ElementType } from 'react'
 
 // ** Next Imports
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 // ** MUI Imports
-import Chip from '@mui/material/Chip';
-import ListItem from '@mui/material/ListItem';
-import Typography from '@mui/material/Typography';
-import Box, { BoxProps } from '@mui/material/Box';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import { styled, useTheme } from '@mui/material/styles';
-import ListItemButton, { ListItemButtonProps } from '@mui/material/ListItemButton';
+import Chip from '@mui/material/Chip'
+import ListItem from '@mui/material/ListItem'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import Box, { BoxProps } from '@mui/material/Box'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemButton, { ListItemButtonProps } from '@mui/material/ListItemButton'
+import Icon from '@/@core/components/icon';
 
 // ** Configs Import
-import themeConfig from '@/configs/themeConfig';
+import themeConfig from '@/configs/themeConfig'
 
 // ** Types
-import { NavLink, NavGroup } from '@/@core/layouts/types';
-import { Settings } from '@/@core/context/settingsContext';
+import { NavLink, NavGroup } from '@/@core/layouts/types'
+import { Settings } from '@/@core/context/settingsContext'
 
 // ** Custom Components Imports
-import UserIcon from '@/layouts/components/UserIcon';
-import Translations from '@/layouts/components/Translations';
-import CanViewNavLink from '@/layouts/components/acl/CanViewNavLink';
+import UserIcon from '@/layouts/components/UserIcon'
+import Translations from '@/layouts/components/Translations'
+import CanViewNavLink from '@/layouts/components/acl/CanViewNavLink'
 
-// ** Utils
-import { handleURLQueries } from '@/@core/layouts/utils';
+// ** Util Import
+import { handleURLQueries } from '@/@core/layouts/utils'
 
 interface Props {
-  parent?: boolean;
-  item: NavLink;
-  navHover?: boolean;
-  settings: Settings;
-  navVisible?: boolean;
-  collapsedNavWidth: number;
-  navigationBorderWidth: number;
-  toggleNavVisibility: () => void;
-  isSubToSub?: NavGroup | undefined;
+  parent?: boolean
+  item: NavLink
+  navHover?: boolean
+  settings: Settings
+  navVisible?: boolean
+  collapsedNavWidth: number
+  navigationBorderWidth: number
+  toggleNavVisibility: () => void
+  isSubToSub?: NavGroup | undefined
 }
 
 // ** Styled Components
 const MenuNavLink = styled(ListItemButton)<
-  ListItemButtonProps & {
-    component?: ElementType;
-    target?: '_blank' | undefined;
-  }
+  ListItemButtonProps & { component?: ElementType; href: string; target?: '_blank' | undefined }
 >(({ theme }) => ({
   width: '100%',
   borderTopRightRadius: 100,
@@ -56,26 +54,22 @@ const MenuNavLink = styled(ListItemButton)<
   '&.active': {
     '&, &:hover': {
       boxShadow: theme.shadows[3],
-      backgroundImage: `linear-gradient(98deg, ${theme.palette.customColors.primaryGradient}, ${theme.palette.primary.main} 94%)`,
+      backgroundImage: `linear-gradient(98deg, ${theme.palette.customColors.primaryGradient}, ${theme.palette.primary.main} 94%)`
     },
     '& .MuiTypography-root, & .MuiListItemIcon-root': {
-      color: `${theme.palette.common.white} !important`,
-    },
-  },
-}));
+      color: `${theme.palette.common.white} !important`
+    }
+  }
+}))
 
-const MenuItemTextMetaWrapper = styled(Box)<BoxProps>({
+const MenuItemTextMetaWrapper = styled(Box)<BoxProps>(({ theme }) => ({
   width: '100%',
   display: 'flex',
   alignItems: 'center',
+  gap: theme.spacing(2),
   justifyContent: 'space-between',
   transition: 'opacity .25s ease-in-out',
-  ...(themeConfig.menuTextTruncate && { overflow: 'hidden' }),
-});
-
-const LinkStyled = styled(Link)(({ theme }) => ({
-  textDecoration: 'none',
-  color: theme.palette.primary.main
+  ...(themeConfig.menuTextTruncate && { overflow: 'hidden' })
 }))
 
 const VerticalNavLink = ({
@@ -87,42 +81,23 @@ const VerticalNavLink = ({
   isSubToSub,
   collapsedNavWidth,
   toggleNavVisibility,
-  navigationBorderWidth,
+  navigationBorderWidth
 }: Props) => {
   // ** Hooks
-  const theme = useTheme();
-  const router = useRouter();
+  const router = useRouter()
 
   // ** Vars
-  const { skin, navCollapsed } = settings;
+  const { navCollapsed } = settings
 
-  const IconTag: ReactNode = parent && !item.icon ? themeConfig.navSubItemIcon : item.icon;
-
-  const conditionalBgColor = () => {
-    if (skin === 'semi-dark' && theme.palette.mode === 'light') {
-      return {
-        color: `rgba(${theme.palette.customColors.dark}, 0.87)`,
-        '&:hover': {
-          backgroundColor: `rgba(${theme.palette.customColors.dark}, 0.04)`,
-        },
-      };
-    } else if (skin === 'semi-dark' && theme.palette.mode === 'dark') {
-      return {
-        color: `rgba(${theme.palette.customColors.light}, 0.87)`,
-        '&:hover': {
-          backgroundColor: `rgba(${theme.palette.customColors.light}, 0.04)`,
-        },
-      };
-    } else return {};
-  };
+  const icon = parent && !item.icon ? themeConfig.navSubItemIcon : item.icon
 
   const isNavLinkActive = () => {
     if (router.pathname === item.path || handleURLQueries(router, item.path)) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
-  };
+  }
 
   return (
     <CanViewNavLink navLink={item}>
@@ -132,84 +107,75 @@ const VerticalNavLink = ({
         disabled={item.disabled || false}
         sx={{ mt: 1.5, px: '0 !important' }}
       >
-        <LinkStyled passHref href={item.path === undefined ? '/' : `${item.path}`}>
-          <MenuNavLink
-            className={isNavLinkActive() ? 'active' : ''}
-            {...(item.openInNewTab ? { target: '_blank' } : null)}
-            onClick={(e) => {
-              if (item.path === undefined) {
-                e.preventDefault();
-                e.stopPropagation();
-              }
-              if (navVisible) {
-                toggleNavVisibility();
-              }
-            }}
-            sx={{
-              py: 2.25,
-              ...conditionalBgColor(),
-              ...(item.disabled ? { pointerEvents: 'none' } : { cursor: 'pointer' }),
-              pl: navCollapsed && !navHover ? (collapsedNavWidth - navigationBorderWidth - 24) / 8 : 5.5,
-              pr: navCollapsed && !navHover ? ((collapsedNavWidth - navigationBorderWidth - 24) / 2 - 5) / 4 : 3.5,
-            }}
-          >
-            {isSubToSub ? null : (
-              <ListItemIcon
-                sx={{
-                  color: 'text.primary',
-                  transition: 'margin .25s ease-in-out',
-                  ...(navCollapsed && !navHover ? { mr: 0 } : { mr: 2.5 }),
-                  ...(parent ? { ml: 1.25, mr: 3.75 } : {}), // This line should be after (navCollapsed && !navHover) condition for proper styling
-                }}
-              >
-                <UserIcon
-                  icon={IconTag}
-                  componentType='vertical-menu'
-                  iconProps={{
-                    sx: {
-                      fontSize: '0.875rem',
-                      ...(!parent ? { fontSize: '1.5rem' } : {}),
-                      ...(parent && item.icon ? { fontSize: '0.875rem' } : {}),
-                    },
-                  }}
-                />
-              </ListItemIcon>
-            )}
-
-            <MenuItemTextMetaWrapper
+        <MenuNavLink
+          component={Link}
+          {...(item.disabled && { tabIndex: -1 })}
+          className={isNavLinkActive() ? 'active' : ''}
+          href={item.path === undefined ? '/' : `${item.path}`}
+          {...(item.openInNewTab ? { target: '_blank' } : null)}
+          onClick={e => {
+            if (item.path === undefined) {
+              e.preventDefault()
+              e.stopPropagation()
+            }
+            if (navVisible) {
+              toggleNavVisibility()
+            }
+          }}
+          sx={{
+            py: 2.25,
+            ...(item.disabled ? { pointerEvents: 'none' } : { cursor: 'pointer' }),
+            pl: navCollapsed && !navHover ? (collapsedNavWidth - navigationBorderWidth - 24) / 8 : 5.5,
+            pr: navCollapsed && !navHover ? ((collapsedNavWidth - navigationBorderWidth - 24) / 2 - 5) / 4 : 3.5
+          }}
+        >
+          {isSubToSub ? null : (
+            <ListItemIcon
               sx={{
-                ...(isSubToSub ? { ml: 9 } : {}),
-                ...(navCollapsed && !navHover ? { opacity: 0 } : { opacity: 1 }),
+                color: 'text.primary',
+                transition: 'margin .25s ease-in-out',
+                ...(navCollapsed && !navHover ? { mr: 0 } : { mr: 2.5 }),
+                ...(parent ? { ml: 1.25, mr: 3.75 } : {}), // This line should be after (navCollapsed && !navHover) condition for proper styling
+                '& svg': {
+                  fontSize: '0.875rem',
+                  ...(!parent ? { fontSize: '1.5rem' } : {}),
+                  ...(parent && item.icon ? { fontSize: '0.875rem' } : {})
+                }
               }}
             >
-              <Typography
-                {...((themeConfig.menuTextTruncate || (!themeConfig.menuTextTruncate && navCollapsed && !navHover)) && {
-                  noWrap: true,
-                })}
-              >
-                <Translations text={item.title} />
-              </Typography>
-              {item.badgeContent ? (
-                <Chip
-                  label={item.badgeContent}
-                  color={item.badgeColor || 'primary'}
-                  sx={{
-                    ml: 1.25,
-                    height: 20,
-                    fontWeight: 500,
-                    '& .MuiChip-label': {
-                      px: 1.5,
-                      textTransform: 'capitalize',
-                    },
-                  }}
-                />
-              ) : null}
-            </MenuItemTextMetaWrapper>
-          </MenuNavLink>
-        </LinkStyled>
+              <Icon icon={icon as string} />
+            </ListItemIcon>
+          )}
+
+          <MenuItemTextMetaWrapper
+            sx={{
+              ...(isSubToSub ? { ml: 9 } : {}),
+              ...(navCollapsed && !navHover ? { opacity: 0 } : { opacity: 1 })
+            }}
+          >
+            <Typography
+              {...((themeConfig.menuTextTruncate || (!themeConfig.menuTextTruncate && navCollapsed && !navHover)) && {
+                noWrap: true
+              })}
+            >
+              <Translations text={item.title} />
+            </Typography>
+            {item.badgeContent ? (
+              <Chip
+                label={item.badgeContent}
+                color={item.badgeColor || 'primary'}
+                sx={{
+                  height: 20,
+                  fontWeight: 500,
+                  '& .MuiChip-label': { px: 1.5, textTransform: 'capitalize' }
+                }}
+              />
+            ) : null}
+          </MenuItemTextMetaWrapper>
+        </MenuNavLink>
       </ListItem>
     </CanViewNavLink>
-  );
-};
+  )
+}
 
-export default VerticalNavLink;
+export default VerticalNavLink
