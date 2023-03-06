2e4859e442f667d6b067b9f729140421d8daea9f
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-06 21:49:37 +0700

    Add StudentCheckInReport page

---
+++ frontend/src/pages/apps/reports/student/check-in-report/index.tsx
@@ -0,0 +1,103 @@
+import 'dayjs/locale/th';
+
+// ** Types Imports
+import { Avatar, Card, CardHeader, Grid } from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
+// ** React Imports
+import { useEffect, useState } from 'react';
+
+import { BsCalendar2Date } from 'react-icons/bs';
+import { LocalStorageService } from '@/services/localStorageService';
+import { ReportCheckIn } from '@/types/apps/reportCheckIn';
+import Spinner from '@/@core/components/spinner';
+
+import buddhistEra from 'dayjs/plugin/buddhistEra';
+import { isEmpty } from '@/@core/utils/utils';
+import { shallow } from 'zustand/shallow';
+import { useActivityCheckInStore, useReportCheckInStore } from '@/store/index';
+import { useAuth } from '@/hooks/useAuth';
+import useGetFullNameWithTitle from '@/hooks/useFullNameWithTitle';
+import TableHeaderWeekly from '@/views/apps/reports/student/TableHeaderWeekly';
+import TableSpanning from '@/views/apps/reports/student/TableSpanning';
+
+dayjs.locale('th');
+dayjs.extend(buddhistEra);
+
+const localStorageService = new LocalStorageService();
+const storedToken = localStorageService.getToken()!;
+
+const StudentCheckInReport = () => {
+  const { user } = useAuth();
+  const { fullName } = useGetFullNameWithTitle({ user } as any);
+
+  // ** Store Vars
+  const { findStudentWeeklyReport }: any = useReportCheckInStore(
+    (state) => ({
+      findStudentWeeklyReport: state.findStudentWeeklyReport,
+    }),
+    shallow,
+  );
+
+  // ** State
+  const [value, setValue] = useState<ReportCheckIn>({} as ReportCheckIn);
+  const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs(new Date()));
+  const [startOfWeek, setStartOfWeek] = useState<Dayjs>(dayjs(new Date()).startOf('week').add(1, 'day'));
+  const [endOfWeek, setEndOfWeek] = useState<Dayjs>(dayjs(new Date()).endOf('week').subtract(1, 'day'));
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetch = async () => {
+      setLoading(true);
+      const start = selectedDate.startOf('week').add(1, 'day');
+      setStartOfWeek(start);
+      const end = selectedDate.endOf('week').subtract(1, 'day');
+      setEndOfWeek(end);
+      await findStudentWeeklyReport(storedToken, {
+        studentId: user?.id,
+        classroomId: user?.student?.classroom?.id,
+        start,
+        end,
+      }).then(async (res: any) => {
+        setValue(await res);
+        setLoading(false);
+      });
+    };
+    fetch();
+  }, [selectedDate]);
+
+  const handleSelectedDate = (date: Dayjs | null) => {
+    setSelectedDate(date as Dayjs);
+  };
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ color: 'primary.main' }} aria-label='recipe'>
+                <BsCalendar2Date />
+              </Avatar>
+            }
+            sx={{ color: 'text.primary' }}
+            title={`รายงานการเช็คชื่อของ ${fullName} ชั้น ${user?.student?.classroom?.name} `}
+            subheader={`ประจำสัปดาห์ที่ ${startOfWeek.format('DD MMMM BBBB')} - ${endOfWeek.format('DD MMMM BBBB')}`}
+          />
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <TableHeaderWeekly selectedDate={selectedDate} handleSelectedDate={handleSelectedDate} />
+          {loading ? <Spinner /> : <TableSpanning value={value} />}
+        </Card>
+      </Grid>
+    </Grid>
+  );
+};
+
+StudentCheckInReport.acl = {
+  action: 'read',
+  subject: 'student-check-in-report',
+};
+
+export default StudentCheckInReport;
