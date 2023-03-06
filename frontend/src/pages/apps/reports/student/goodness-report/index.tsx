242e723cbe69b0b0bab5e4267e7194ec12780a41
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-06 21:49:45 +0700

    Add StudentGoodnessReport page and acl

---
+++ frontend/src/pages/apps/reports/student/goodness-report/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const StudentGoodnessReport = (props: Props) => {
+  return <div>StudentGoodnessReport </div>;
+};
+
+StudentGoodnessReport.acl = {
+  action: 'read',
+  subject: 'student-goodness-report',
+}
+export default StudentGoodnessReport;
