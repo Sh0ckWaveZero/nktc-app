e0201c90977b40c9bc0173d9201796a302684127
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-06 14:42:39 +0700

    Commit message: Add programs to database using data from program worksheet

--- backend/src/database/db/program.ts
+++ backend/src/database/db/program.ts
@@ -1,25 +1,30 @@
-import { Prisma } from '@prisma/client';
-import { createByAdmin, getLevelByName, readWorkSheetFromFile } from '../../utils/utils';
+import { Prisma, PrismaClient } from '@prisma/client';
+import { createByAdmin, getLevelId, readWorkSheetFromFile } from '../../utils/utils';
 
+const prisma = new PrismaClient();
 export const programData = async () => {
   const workSheetsFromFile = readWorkSheetFromFile('program');
   const admin = createByAdmin();
 
-  return await Promise.all(workSheetsFromFile[0].data
+  const programs = await Promise.all(workSheetsFromFile[0].data
     .filter((data: any, id: number) => id > 1 && data)
     .map(async (item: any) => {
       const programId = item[0].toString();
       const name = item[1].toString();
       const description = item[2].toString();
-      const level = await getLevelByName(item[3].toString());
-      return Prisma.validator<Prisma.ProgramCreateInput>()(
+      const level = await getLevelId(item[3].toString());
+      return Prisma.validator<Prisma.ProgramCreateManyInput>()(
         {
           programId,
           name,
           description,
-          ...level,
+          levelId: level?.id || '',
           ...admin,
         },
       );
     }));
+  await prisma.program.createMany({
+    data: programs,
+  });
 }
+
