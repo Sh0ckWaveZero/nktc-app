8e662762b0bd6a93586a8a34a7c7807d1b152ec8
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-09 23:36:36 +0700

    Add modifyPdf function to utils to draw text on PDF document

---
+++ frontend/src/utils/modifyPdf.ts
@@ -0,0 +1,55 @@
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+import fontkit from '@pdf-lib/fontkit'
+import SarabunBase64 from './font-base64';
+
+async function modifyPdf(blob: any, user: any) {
+  const pdfDoc = await PDFDocument.load(blob);
+  pdfDoc.registerFontkit(fontkit);
+  const helveticaFont = await pdfDoc.embedFont(SarabunBase64.regular);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  const { width, height } = firstPage.getSize();
+  const fullName = `${user?.account?.firstName} ${user?.account?.lastName}`;
+  const educationsYears = process.env.NEXT_PUBLIC_EDUCATION_YEARS || '';
+  const currentDate = new Date().toLocaleDateString('th-TH', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+  const textSize = 46;
+  const textWidth = helveticaFont.widthOfTextAtSize(fullName, textSize);
+  const textCurrentDate = helveticaFont.widthOfTextAtSize(currentDate, textSize);
+  const textHeight = helveticaFont.heightAtSize(textSize);
+
+  firstPage.drawText(fullName, {
+    x: width / 2 - textWidth / 2,
+    y: height / 2,
+    size: 46,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(0),
+  });
+
+  firstPage.drawText(educationsYears, {
+    x: width / 2 +  520,
+    y: height / 2 - 123,
+    size: 40,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(0),
+  });
+
+  firstPage.drawText(currentDate, {
+    x: width / 2 - textCurrentDate / 2 ,
+    y: height / 2 - 205,
+    size: 40,
+    font: helveticaFont,
+    color: rgb(0.95, 0.1, 0.1),
+    rotate: degrees(0),
+  });
+  const pdfBytes = await pdfDoc.save();
+
+  return pdfBytes;
+}
+
+export default modifyPdf;
