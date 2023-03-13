77a409abc547e76a5cf04e2851389f8faeeef1ed
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-13 22:36:02 +0700

    Add summary() method to BadnessIndividualState interface and implement it in the store

--- frontend/src/store/apps/badness-individual/index.ts
+++ frontend/src/store/apps/badness-individual/index.ts
@@ -15,6 +15,7 @@ interface BadnessIndividualState {
   createBadnessIndividual: (token: string, body: any) => any;
   createBadnessGroup: (token: string, body: any) => any;
   search: (token: string, body: Body) => any;
+  summary : (token: string, body: any) => any;
 }
 
 export const badnessIndividualStore = create<BadnessIndividualState>()(
@@ -64,6 +65,22 @@ export const badnessIndividualStore = create<BadnessIndividualState>()(
         return response?.data;
       } catch (err) {
         console.error('Error creating badness individual:', err);
+        return err;
+      }
+    },
+    summary: async (token: string, body: any) => {
+      try {
+        const response = await httpClient.post(
+          `${authConfig.badnessIndividualEndpoint}/summary`,
+          body,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        return response?.data;
+      } catch (err) {
+        console.error('Error get summary:', err);
         return err;
       }
     },
