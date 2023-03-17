dc686a3783ba4a8e25a147115aba4476c9ae5d00
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-17 21:13:01 +0700

    refactor: 💡 un-use code, lib, and component

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,29 +2,15 @@ const path = require('path')
 
 /** @type {import('next').NextConfig} */
 
-// Remove this if you're not using Fullcalendar features
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid',
-])
-
-module.exports = withTM({
-  // trailingSlash: true,
-  // reactStrictMode: false,
-  // experimental: {
-  //   esmExternals: false,
-  //   jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
-  // },
+module.exports = {
+  trailingSlash: true,
+  reactStrictMode: false,
   output: 'standalone',
-  webpack: (config) => {
+  webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision'),
     }
 
     return config
-  },
-})
+  }
+}
