0990e3152ef364afbbbdc15eb28233a2d6b1cd55
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-21 22:29:35 +0700

    Add fetchBadnessIndividualById method to BadnessIndividualState interface and implement it in the store

--- frontend/src/store/apps/badness-individual/index.ts
+++ frontend/src/store/apps/badness-individual/index.ts
@@ -15,8 +15,9 @@ interface BadnessIndividualState {
   createBadnessIndividual: (token: string, body: any) => any;
   createBadnessGroup: (token: string, body: any) => any;
   search: (token: string, body: Body) => any;
-  summary : (token: string, body: any) => any;
+  summary: (token: string, body: any) => any;
   deleteBadnessIndividualById: (token: string, id: string) => any;
+  fetchBadnessIndividualById: (token: string, body: Body) => any;
 }
 
 export const badnessIndividualStore = create<BadnessIndividualState>()(
@@ -97,6 +98,21 @@ export const badnessIndividualStore = create<BadnessIndividualState>()(
         return response;
       } catch (err) {
         console.error('Error deleting badness individual:', err);
+        return err;
+      }
+    },
+    fetchBadnessIndividualById: async (token: string, body: Body) => {
+      try {
+        const response = await httpClient.get(
+          `${authConfig.badnessIndividualEndpoint}/${body.studentId}?skip=${body.skip}&take=${body.take}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+        return response?.data;
+      } catch (err) {
+        console.error('Error fetching badness individual:', err);
         return err;
       }
     }
