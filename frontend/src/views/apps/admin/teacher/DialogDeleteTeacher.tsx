d9e75880cb14d7a2e25524ed220338a10f8d0b75
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-03-21 23:23:44 +0700

    Add new file DialogDeleteTeacher.tsx for deleting teacher data in the admin app

---
+++ frontend/src/views/apps/admin/teacher/DialogDeleteTeacher.tsx
@@ -0,0 +1,73 @@
+import IconifyIcon from '@/@core/components/icon';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  styled,
+} from '@mui/material';
+import React from 'react';
+
+type Props = {
+  data: any;
+  onClose: () => void;
+  onSubmitted: () => void;
+  open: boolean;
+};
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+const DialogDeleteTeacher = (props: Props) => {
+  const { data, onClose, onSubmitted, open } = props;
+  const fullName = `${data?.title ? data?.title : ''}${data?.firstName} ${data?.lastName}`;
+  return (
+    <BootstrapDialog
+      fullWidth
+      maxWidth='xs'
+      onClose={onClose}
+      aria-labelledby='ยืนยันการลบข้อมูลครู / บุคลากร'
+      open={open}
+    >
+      {onClose ? (
+        <IconButton
+          aria-label='close'
+          onClick={onClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <IconifyIcon icon='mdi:close' />
+        </IconButton>
+      ) : null}
+      <DialogTitle id='alert-dialog-title-goodness'>ยืนยันการลบข้อมูลครู / บุคลากร</DialogTitle>
+      <DialogContent>
+        <DialogContentText id='alert-delete-badness' p={5}>
+          {`คุณต้องการลบข้อมูลของ ${fullName} ใช่หรือไม่?`}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions className='dialog-badness-dense'>
+        <Button color='secondary' onClick={onClose}>
+          ยกเลิก
+        </Button>
+        <Button variant='contained' color='error' onClick={onSubmitted}>
+          ยืนยัน
+        </Button>
+      </DialogActions>
+    </BootstrapDialog>
+  );
+};
+
+export default DialogDeleteTeacher;
