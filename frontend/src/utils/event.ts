2f8e16192017e03e99b6740df465dabbd0f5bdd8
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-04-01 11:13:03 +0700

    🐛 fix(event.ts): refactor handleKeyDown function to use allowedKeys array instead of multiple if statements 🔥 chore(event.ts): remove unused generateErrorMessages interface and object

--- frontend/src/utils/event.ts
+++ frontend/src/utils/event.ts
@@ -1,23 +1,12 @@
-export const handleKeyDown = (e: any) => {
-  // allow RegExp only number english keyboard control key copy and paste
+export const handleKeyDown = (e: KeyboardEvent) => {
+  const allowedKeys = ['Backspace', 'Tab', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Delete', 'Control', 'c', 'v', 'x'];
   const regex = new RegExp('^[0-9]+$');
-  if (
-    !regex.test(e.key) &&
-    e.key !== 'Backspace' &&
-    e.key !== 'Tab' &&
-    e.key !== 'ArrowLeft' &&
-    e.key !== 'ArrowRight' &&
-    e.key !== 'ArrowUp' &&
-    e.key !== 'ArrowDown' &&
-    e.key !== 'Delete' &&
-    e.key !== 'Control' &&
-    e.key !== 'c' &&
-    e.key !== 'v' &&
-    e.key !== 'x'
-  ) {
+
+  if (!regex.test(e.key) && !allowedKeys.includes(e.key)) {
     e.preventDefault();
   }
 };
+
 
 interface Errors {
   [key: string]: string;
@@ -26,4 +15,3 @@ interface Errors {
 export const generateErrorMessages: Errors = {
   'request entity too large': 'ขนาดไฟล์ใหญ่เกินไป',
 }
-
