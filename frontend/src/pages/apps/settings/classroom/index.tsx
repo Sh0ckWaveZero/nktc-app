3ab7b7592ae7d89e433f665b9211474f22066e9c
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-05-13 21:25:35 +0700

    ✨ feat(classroom/index.tsx): add ClassroomList component with table, filter, pagination, and delete functionality. Also add AddClassroomDrawer and DialogDeleteClassroom components.

---
+++ frontend/src/pages/apps/settings/classroom/index.tsx
@@ -0,0 +1,307 @@
+import { Card, CardHeader, Grid, IconButton, Menu, MenuItem, Typography } from '@mui/material';
+import {
+  DeleteOutline,
+  DotsVertical,
+} from 'mdi-material-ui';
+import { Fragment, MouseEvent, useCallback, useEffect, useState } from 'react';
+
+import AddClassroomDrawer from '@/views/apps/settings/classroom/AddClassroomDrawer';
+import CustomNoRowsOverlay from '@/@core/components/check-in/CustomNoRowsOverlay';
+import { DataGrid } from '@mui/x-data-grid';
+import DialogDeleteClassroom from '@/views/apps/settings/classroom/DialogDeleteClassroom';
+import { LocalStorageService } from '@/services/localStorageService';
+import TableHeader from '@/views/apps/settings/classroom/TableHeader';
+import { generateErrorMessages } from 'utils/event';
+import { shallow } from 'zustand/shallow';
+import toast from 'react-hot-toast';
+import { useAuth } from '../../../../hooks/useAuth';
+import { useClassroomStore } from '@/store/index';
+import { useDebounce } from '@/hooks/userCommon';
+
+interface RowOptionsType {
+  row: any;
+  handleDelete: (data: any) => void;
+}
+
+interface CellType {
+  row: any;
+}
+
+const localStorageService = new LocalStorageService();
+const accessToken = localStorageService.getToken()!;
+
+const RowOptions = ({ row, handleDelete }: RowOptionsType) => {
+  // ** State
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const rowOptionsOpen = Boolean(anchorEl);
+  const handleRowOptionsClick = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleRowOptionsClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDeleteRow = () => {
+    handleDelete(row);
+    handleRowOptionsClose();
+  };
+
+  return (
+    <>
+      <IconButton size='small' onClick={handleRowOptionsClick}>
+        <DotsVertical />
+      </IconButton>
+      <Menu
+        keepMounted
+        anchorEl={anchorEl}
+        open={rowOptionsOpen}
+        onClose={handleRowOptionsClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        PaperProps={{ style: { minWidth: '8rem' } }}
+      >
+        <MenuItem onClick={handleDeleteRow}>
+          <DeleteOutline fontSize='small' sx={{ mr: 2, color: 'error.main' }} />
+          ลบ
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+const ClassroomList = () => {
+  // ** Local State
+  const [value, setValue] = useState<string>('');
+
+  const [addUserOpen, setAddUserOpen] = useState<boolean>(false);
+  const debouncedValue = useDebounce<string>(value, 500);
+  const [openDialogDelete, setOpenDialogDelete] = useState(false);
+  const [currentClassroom, setCurrentClassroom] = useState<any>(null);
+  const [classrooms, setClassrooms] = useState<any>([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [isAddUser, setIsAddUser] = useState(false);
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  // ** Hooks
+  const { user } = useAuth();
+
+  const { createClassroom, fetchClassrooms, removeClassrooms }: any = useClassroomStore(
+    (state) => ({
+      classroom: state.classroom,
+      createClassroom: state.createClassroom,
+      fetchClassrooms: state.fetchClassrooms,
+      removeClassrooms: state.removeClassrooms,
+    }),
+    shallow,
+  );
+
+  const searchWithParams = async (params: any) => {
+    try {
+      setLoading(true);
+
+      const response = await fetchClassrooms(accessToken, {
+        name: debouncedValue,
+        ...params,
+      });
+
+      setClassrooms(response?.data || []);
+      setTotal(response?.total);
+      setLoading(false);
+    } catch (error: any) {
+      toast.error(error?.message);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const params = {
+      skip: page === 0 ? 0 : page * pageSize,
+      take: pageSize,
+    };
+    searchWithParams(params);
+  }, [debouncedValue, isEdit, isAddUser, isDelete, page, pageSize]);
+
+  const handleFilter = useCallback((val: string) => {
+    setValue(val);
+  }, []);
+
+  const toggleAddClassroomDrawer = () => setAddUserOpen(!addUserOpen);
+
+  const handleDelete = (data: any) => {
+    setCurrentClassroom(data);
+    setOpenDialogDelete(true);
+  };
+
+  const onHandleAddClassroom = async (info: any) => {
+    setAddUserOpen(false);
+    setIsAddUser(false);
+    const body = {
+      ...info,
+      createdBy: user?.id,
+      updatedBy: user?.id,
+    };
+    const toastId = toast.loading('กำลังเพิ่มข้อมูลขอห้องเรียน...');
+    await createClassroom(accessToken, body).then((res: any) => {
+      if (res?.name !== 'AxiosError') {
+        toast.success('เพิ่มข้อมูลสำเร็จ', { id: toastId });
+        setIsAddUser(true);
+      } else {
+        const { data } = res?.response || {};
+        const message = generateErrorMessages[data?.message] || data?.message;
+        toast.error(message || 'เกิดข้อผิดพลาด', { id: toastId });
+      }
+    });
+  };
+
+  const handleDeleteClose = () => {
+    setOpenDialogDelete(false);
+  };
+
+  const handleDeleteConfirm = async () => {
+    setOpenDialogDelete(false);
+
+    const toastId = toast.loading('กำลังลบข้อมูลขอห้องเรียน...');
+    await removeClassrooms(accessToken, currentClassroom?.id).then((res: any) => {
+      if (res?.name !== 'AxiosError') {
+        toast.success('ลบข้อมูลสำเร็จ', { id: toastId });
+        setIsDelete(true);
+      } else {
+        const { data } = res?.response || {};
+        const message = generateErrorMessages[data?.message] || data?.message;
+        toast.error(message || 'เกิดข้อผิดพลาด', { id: toastId });
+      }
+    });
+  };
+
+  const onHandleChangePage = useCallback((newPage: any) => {
+    setPageSize(newPage);
+  }, []);
+
+  const columns: any = [
+    {
+      flex: 0.1,
+      minWidth: 50,
+      field: 'classroomId',
+      headerName: 'รหัสห้องเรียน',
+      editable: false,
+      sortable: false,
+      hideSortIcons: true,
+      renderCell: ({ row }: CellType) => {
+        const { classroomId } = row;
+        return <Typography variant='body2'>{classroomId}</Typography>;
+      },
+    },
+    {
+      flex: 0.25,
+      minWidth: 200,
+      field: 'name',
+      headerName: 'ชื่อห้องเรียน',
+      editable: false,
+      sortable: false,
+      hideSortIcons: true,
+      renderCell: ({ row }: CellType) => {
+        const { name } = row;
+        return <Typography variant='body2'>{name}</Typography>;
+      },
+    },
+    {
+      flex: 0.1,
+      minWidth: 100,
+      field: 'level',
+      headerName: 'ระดับชั้น',
+      editable: false,
+      sortable: false,
+      hideSortIcons: true,
+      renderCell: ({ row }: CellType) => {
+        const { level } = row;
+        return <Typography variant='body2'>{level?.levelName}</Typography>;
+      },
+    },
+    {
+      flex: 0.1,
+      minWidth: 100,
+      field: 'department',
+      headerName: 'แผนก',
+      editable: false,
+      sortable: false,
+      hideSortIcons: true,
+      renderCell: ({ row }: CellType) => {
+        const { department } = row;
+        return <Typography variant='body2'>{department?.name}</Typography>;
+      },
+    },
+    {
+      flex: 0.15,
+      minWidth: 90,
+      sortable: false,
+      field: 'actions',
+      headerName: 'การดำเนินการอื่น ๆ',
+      editable: false,
+      hideSortIcons: true,
+      align: 'right',
+      renderCell: ({ row }: CellType) => <RowOptions row={row} handleDelete={handleDelete} />,
+    },
+  ];
+
+  return (
+    <Fragment>
+      <Grid container spacing={6}>
+        <Grid item xs={12}>
+          <Card>
+            <CardHeader title='รายชื่อห้องเรียน' />
+            <TableHeader value={value} handleFilter={handleFilter} toggle={toggleAddClassroomDrawer} />
+            <DataGrid
+              disableColumnMenu
+              autoHeight={true}
+              rows={classrooms}
+              getRowHeight={() => 'auto'}
+              columns={columns}
+              loading={loading}
+              pagination
+              paginationMode='server'
+              pageSize={pageSize}
+              rowCount={total}
+              onPageChange={(params: any) => setPage(params)}
+              rowsPerPageOptions={[5, 10, 20, 50]}
+              onPageSizeChange={(newPageSize: number) => onHandleChangePage(newPageSize)}
+              components={{
+                NoRowsOverlay: CustomNoRowsOverlay,
+              }}
+            />
+          </Card>
+        </Grid>
+        {addUserOpen && (
+          <AddClassroomDrawer
+            open={addUserOpen}
+            toggle={toggleAddClassroomDrawer}
+            onSubmitForm={onHandleAddClassroom}
+          />
+        )}
+      </Grid>
+      {openDialogDelete && (
+        <DialogDeleteClassroom
+          data={currentClassroom}
+          onClose={handleDeleteClose}
+          onSubmitted={handleDeleteConfirm}
+          open={openDialogDelete}
+        />
+      )}
+    </Fragment>
+  );
+};
+
+ClassroomList.acl = {
+  action: 'manage',
+  subject: 'settings-classroom-list-pages',
+};
+export default ClassroomList;
