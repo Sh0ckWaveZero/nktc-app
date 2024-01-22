b18ce7b2df9e5ae80227327dfe61327260a8a036
Aphidet-Hongseetong <ottooffple@icloud.com>
2024-01-22 20:32:31 +0700

    Update getTracker method to be asynchronous

--- backend/src/common/guards/throttler-behind-proxy.guard.ts
+++ backend/src/common/guards/throttler-behind-proxy.guard.ts
@@ -4,7 +4,7 @@ import * as requestIp from 'request-ip';
 
 @Injectable()
 export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
-  protected getTracker(req: Record<string, unknown>): string {
+  protected async getTracker(req: Record<string, any>): Promise<string> {
     return (
       requestIp.getClientIp(req as unknown as requestIp.Request) ?? 'null-ip'
     );
