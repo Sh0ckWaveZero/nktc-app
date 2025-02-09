55561b747ed36f7a627efb8ad9b3b3baa7c2c85c
Aphidet-Hongseetong <ottooffple@icloud.com>
2025-02-09 22:32:31 +0700

    Remove bun.lockb file and update PrismaService to include a close method; update package dependencies for NestJS and other libraries

--- backend/src/common/services/prisma-mongodb.service.ts
+++ backend/src/common/services/prisma-mongodb.service.ts
@@ -7,9 +7,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+  async close() {
+    await this.$disconnect();
   }
 }
